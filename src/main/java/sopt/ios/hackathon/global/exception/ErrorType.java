4028a21b6a9d0c9f73a2fdb945f6ddc67190a63f
gahyuun <pgh123166@naver.com>
2024-11-23 22:22:03 +0900

    chore: error code 수정

--- src/main/java/sopt/ios/hackathon/global/exception/ErrorType.java
+++ src/main/java/sopt/ios/hackathon/global/exception/ErrorType.java
@@ -6,12 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorType {
 
     INVALID_FIELD_ERROR(HttpStatus.BAD_REQUEST, "40002", "요청 본문의 필드 값이 허용된 형식과 다릅니다."),
-    NO_REQUEST_PARAMETER_ERROR(HttpStatus.BAD_REQUEST, "40003", "요청에 필요한 파라미터가 존재하지 않습니다."),
     NO_REQUEST_HEADER_ERROR(HttpStatus.BAD_REQUEST, "40004", "요청에 필요한 헤더가 존재하지 않습니다."),
-    TYPE_MISMATCH_ERROR(HttpStatus.BAD_REQUEST, "40005", "잘못된 값이 입력되었습니다."),
-    INVALID_REQUEST_BODY_ERROR(HttpStatus.BAD_REQUEST, "40006", "잘못된 Request Body입니다. 요청 형식 또는 필드를 확인하세요."),
     DATA_INTEGRITY_VIOLATION_ERROR(HttpStatus.BAD_REQUEST, "40007", "데이터 무결성 제약 조건을 위반했습니다."),
-
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "50001", "예상치 못한 서버 에러가 발생했습니다."),
 
     ;
